526b765d8cd7d9ddbeae2b919ef5bfba0a291044
lat9nq <22451773+lat9nq@users.noreply.github.com>
2023-05-28 01:54:17 -0400

    tzdb: Generate nx time zone files

---
+++ src/tzdb/find_tzif.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+set -e
+directory=$1
+tzif_list=$2
+
+truncate -s 0 $tzif_list
+
+for i in `find $directory -type f`; do
+    header=`head -c 4 $i`
+    if [ "$header" == "TZif" ]; then
+        echo "$i" >> $tzif_list
+    fi
+done
+
